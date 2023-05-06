29ebeef65b591227a643c82fac74a285e5510c7c
Andrei Sucman <sucmanandrei@gmail.com>
2023-05-06 11:50:20 +0300

    progress

--- functions/getPasswordMatchHint.js
+++
@@ -1,9 +0,0 @@
-export function getPasswordsMatchHint(passOne, passTwo) {
-  if (!passOne || !passTwo) return false;
-
-  if (passOne !== passTwo) {
-    return "Passwords don't match";
-  } else {
-    return false;
-  }
-}
--- functions/regexValidateEmail.js
+++
@@ -1,5 +0,0 @@
-export function regexValidateEmail(email) {
-  const rule = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
-
-  return email.match(rule);
-}
