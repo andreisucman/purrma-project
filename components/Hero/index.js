213343eed73a199964f50e0c2e8e9eed252c7b2c
Andrei Sucman <sucmanandrei@gmail.com>
2023-05-08 15:13:47 +0300

    hello

--- components/Hero/index.js
+++ components/Hero/index.js
@@ -1,17 +1,6 @@
-import { useState, useEffect } from "react";
-import Image from "next/image";
-import banner from "../../public/assets/banner.webp";
-import banner_mobile from "../../public/assets/banner_mobile.webp";
-import { getDeviceType } from "../../functions/getDeviceType";
 import styles from "./Hero.module.scss";
 
 export default function Hero() {
-  const [deviceType, setDeviceType] = useState();
-
-  useEffect(() => {
-    setDeviceType(getDeviceType());
-  }, []);
-
   return (
     <div className={styles.container}>
       <div className={styles.container__wrapper}>
