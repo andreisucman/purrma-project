ad3b0741ef64f32e088e05c98e80661428027e7d
Andrei Sucman <sucmanandrei@gmail.com>
2023-05-08 15:12:45 +0300

    hello

--- components/Hero/index.js
+++ components/Hero/index.js
@@ -14,29 +14,16 @@ export default function Hero() {
 
   return (
     <div className={styles.container}>
-      {deviceType === "mobile" ? (
-        <Image
-          className={styles.image}
-          width={"auto"}
-          src={banner_mobile}
-          alt="purrma concept schema"
-        />
-      ) : (
-        <Image
-          className={styles.image}
-          width={"auto"}
-          src={banner}
-          alt="purrma concept schema"
-        />
-      )}
-      <div className={styles.slogan}>
-        <h1 className={styles.slogan__first}>
-          <div className="icon icon__dry_food icon_b" />
-          Food and love <div className="icon icon__heart_outline icon_b" />
-        </h1>
-        <h2 className={styles.slogan__second}>
-          For the <span>cats</span> and people
-        </h2>
+      <div className={styles.container__wrapper}>
+        <div className={styles.slogan}>
+          <h1 className={styles.slogan__first}>
+            <div className="icon icon__dry_food icon_b" />
+            Food and love <div className="icon icon__heart_outline icon_b" />
+          </h1>
+          <h2 className={styles.slogan__second}>
+            For the <span>cats</span> and people
+          </h2>
+        </div>
       </div>
     </div>
   );
