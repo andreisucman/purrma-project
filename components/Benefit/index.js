bd6dc7920aa85e22e91c8a18cbfb2135b6e69242
Andrei Sucman <sucmanandrei@gmail.com>
2023-05-08 15:21:32 +0300

    progress

--- components/Benefit/index.js
+++ components/Benefit/index.js
@@ -11,9 +11,9 @@ export default function Benefit() {
           id="benefit"
         />
         <ul className={styles.list}>
-          <li className={styles.list__item}><div className="icon icon__cat_house icon_m"/>Less cats on streets - homeless cats get adopted</li>
+          <li className={styles.list__item}><div className="icon icon__cat_house icon_m"/>Fewer cats on streets - homeless cats get adopted</li>
           <li className={styles.list__item}><div className="icon icon__face icon_m"/>Happier public - everyone can now own a cat</li>
-          <li className={styles.list__item}><div className="icon icon__cat_face icon_m"/>Happier cats - less scratches</li>
+          <li className={styles.list__item}><div className="icon icon__cat_face icon_m"/>Happier cats - fewer scratches</li>
         </ul>
       </div>
     </div>
