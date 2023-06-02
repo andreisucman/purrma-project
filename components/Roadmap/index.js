828b5c0427045abf41703110467069bd33f4b01e
Andrei Sucman <sucmanandrei@gmail.com>
2023-06-02 10:51:59 +0300

    progress

--- components/Roadmap/index.js
+++ components/Roadmap/index.js
@@ -8,14 +8,18 @@ const Roadmap = () => {
     { desc: "Build and test the app", date: "12 Apr 23" },
     { desc: "Launch organically", isCur: true },
     { desc: "Launch the first paid ad campaign", isFut: true },
-    { desc: "Onboard the first 100 sisters", isFut: true },
-    { desc: "Onboard the first 10 mothers", isFut: true },
+    { desc: "Onboard the first 1000 sisters", isFut: true },
+    { desc: "Onboard the first 100 mothers", isFut: true },
   ];
 
   return (
     <div className={styles.container}>
       <div className={styles.container__wrapper}>
-        <SectionTitle icon={"icon icon__map_pin icon_m"} title={"The roadmap"} id="roadmap"/>
+        <SectionTitle
+          icon={"icon icon__map_pin icon_m"}
+          title={"The roadmap"}
+          id="roadmap"
+        />
         <div className={styles.content}>
           {roadmap.map((m, i) => (
             <React.Fragment key={i}>
