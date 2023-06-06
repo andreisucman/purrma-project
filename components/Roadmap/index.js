34d8701ed7b8103a9c4ed6ad0887803fe6696e6a
Andrei Sucman <sucmanandrei@gmail.com>
2023-06-06 16:48:55 +0300

    progress

--- components/Roadmap/index.js
+++ components/Roadmap/index.js
@@ -5,7 +5,7 @@ import styles from "./Roadmap.module.scss";
 
 const Roadmap = () => {
   const roadmap = [
-    { desc: "Build and test the app", date: "12 Apr 23" },
+    { desc: "Build and test the app", date: "Apr 23" },
     { desc: "Find seed capital", isCur: true, date: "Jun 23" },
     { desc: "Launch in the US", isCur: true, date: "Jun 23" },
     {
